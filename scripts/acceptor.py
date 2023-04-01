38dfd0a1d01bdf766c44b7892a4a353e0130f313
olgacode <olgabarlou@gmail.com>
2023-04-01 18:21:07 +0300

    truly fixed

--- scripts/acceptor.py
+++ scripts/acceptor.py
@@ -1,9 +1,3 @@
-"""
-USAGE:
-    python mkfstinput.py MY_WORD > my_word.fst
-OR:
-    python mkfstinput.py MY_WORD | fstcompile | fstcompose - MY_SPELLCHECKER.fst | ...
-"""
 
 import sys
 
@@ -15,13 +9,9 @@ def make_input_fst(word,length):
     This can be composed with other FSTs, e.g. the spell
     checker to provide an "input" word
     """
-    accept_state =  90000
-    #print(format_arc(0, length, c, c, weight=0))
-    #length+=1
     for i, c in enumerate(word):
         if i==0:
-            print(format_arc(1, length, c, word, weight=0))
-        # TODO: You need to implement format_arc function in scripts/util
+            print(format_arc(1, length+1, c, word, weight=0))
         else:
             print(format_arc(length, length + 1, c, EPS, weight=0))
         length += 1
@@ -29,8 +19,6 @@ def make_input_fst(word,length):
         if i == len(word) - 1:
             print(format_arc(length, 0, EPS, EPS, weight=0))
             #print(length-1)
-    #print(0)
-    #print(accept_state)
     return (length)
 
 
@@ -38,10 +26,8 @@ if __name__ == "__main__":
     f=open("words_only.syms","r")
     dictionary=f.readlines()
     dictionary = [s.rstrip() for s in dictionary]
-    #global length
     length = 1
     for word in dictionary:
         length = make_input_fst(word,length)
-        #:length+=len(word)+1
     print("0")
 
