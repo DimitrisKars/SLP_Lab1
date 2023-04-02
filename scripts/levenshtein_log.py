0d7a0514d96a405bec68dd6a5c4f618050ac70d1
olgacode <olgabarlou@gmail.com>
2023-04-03 01:08:39 +0300

    fixed levenshtein alphabet file

--- scripts/levenshtein_log.py
+++ scripts/levenshtein_log.py
@@ -1,4 +1,6 @@
 from util import EPS, format_arc
+import ast
+from math import log
 
 with open("../vocab/chars.syms", 'r') as file:
     data = file.read().replace('\n', '').replace('\t','')
@@ -9,32 +11,41 @@ file_name=open("../vocab/alphabet.syms","w")
 file_name.write(word1)
 
 alphabet = word1
-weight = {
-    "delete": 1.0,
-    "insert": 1.0,
-    "sub": 1.5
-}
+def weight(num):
+    return log(1/num)
 
+with open("./dictionary.txt", 'r') as file:
+    data = file.read()
+frequencies = ast.literal_eval(data)
+
+inf = 1000000
 # No edit
 for l in alphabet:
     print(format_arc(0, 0, l, l))
-    #print("0 0 {} {} {:.3f}".format(l, l, 0))
 
 # Deletes: input character, output epsilon
 for l in alphabet:
-    print(format_arc(0, 0, l,"<eps>", weight["delete"]))
-    #print("0 0 {} <eps> {:.3f}".format(l, weight["delete"]))
+    if (l, '<eps>') not in frequencies:
+        print(format_arc(0, 0, l,"<eps>", inf))
+    else:    
+        print(format_arc(0, 0, l,"<eps>", weight(frequencies[l, '<eps>'])))
 
 # Insertions: input epsilon, output character
 for l in alphabet:
-    print(format_arc(0, 0,"<eps>", l, weight["insert"]))
+    if ('<eps>', l) not in frequencies:
+        print(format_arc(0, 0,"<eps>", l, inf))
+    else:    
+        print(format_arc(0, 0,"<eps>", l, weight(frequencies[('<eps>',l)])))
     #print("0 0 <eps> {} {:.3f}".format(l, weight["insert"]))
 
 # Substitutions: input one character, output another
 for l in alphabet:
     for r in alphabet:
         if l != r:
-            print(format_arc(0, 0, l, r, weight["sub"]))
+            if (l, r) not in frequencies:
+                print(format_arc(0, 0, l, r, inf))
+            else:
+                print(format_arc(0, 0, l, r, weight(frequencies[l,r])))
             #print("0 0 {} {} {:.3f}".format(l, r, weight["sub"]))
 
 # Final state
