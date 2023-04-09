5c419b9545fa3d7581f60fae56c3894359b56f84
olgacode <olgabarlou@gmail.com>
2023-04-09 11:25:54 +0300

    corrected unweighted levenshtein

--- scripts/levenshtein_unweighted.py
+++ scripts/levenshtein_unweighted.py
@@ -1,14 +1,8 @@
 from util import EPS, format_arc
 
-with open("../vocab/chars.syms", 'r') as file:
-    data = file.read().replace('\n', '').replace('\t','')
-import re
-word1 = "".join(re.findall("[a-zA-Z]+", data))
-file_name = 'alphabet.syms'
-file_name=open("../vocab/alphabet.syms","w")
-file_name.write(word1)
+file = open("../vocab/alphabet.syms","r")
+alphabet = file.read()
 
-alphabet = word1
 weight = {
     "delete": 1.0,
     "insert": 1.0,
@@ -18,24 +12,20 @@ weight = {
 # No edit
 for l in alphabet:
     print(format_arc(0, 0, l, l))
-    #print("0 0 {} {} {:.3f}".format(l, l, 0))
 
 # Deletes: input character, output epsilon
 for l in alphabet:
     print(format_arc(0, 0, l,"<eps>", weight["delete"]))
-    #print("0 0 {} <eps> {:.3f}".format(l, weight["delete"]))
 
 # Insertions: input epsilon, output character
 for l in alphabet:
     print(format_arc(0, 0,"<eps>", l, weight["insert"]))
-    #print("0 0 <eps> {} {:.3f}".format(l, weight["insert"]))
 
 # Substitutions: input one character, output another
 for l in alphabet:
     for r in alphabet:
         if l != r:
             print(format_arc(0, 0, l, r, weight["sub"]))
-            #print("0 0 {} {} {:.3f}".format(l, r, weight["sub"]))
 
 # Final state
 print(0)
